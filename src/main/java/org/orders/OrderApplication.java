5a4d5524e4bd00698e6a5acce3599e0a98167e98
Sathish Kumar Parthasarathy <p.sathish.cs@gmail.com>
2017-01-21 14:56:10 -0600

    Adding Consul to the OrdersRS.

--- src/main/java/org/orders/OrderApplication.java
+++ src/main/java/org/orders/OrderApplication.java
@@ -2,7 +2,7 @@ package org.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -12,8 +12,8 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableSwagger2
+@EnableDiscoveryClient
 public class OrderApplication {
 	public static void main(String[] args){
 		SpringApplication.run(OrderApplication.class, args);
